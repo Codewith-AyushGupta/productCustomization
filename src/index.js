9dedd129208fd9825d9db3d2e2d9a0b37ef8491c
prashant-nubys <prashant.kumar@nubystech.com>
2025-08-12 15:59:09 +0530

    Add tab navigation to switch betwwen screens

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AprealCustomizer from "./components/aprealCustomizer";
-import NonAprealCustomizer from "./components/nonAprealCustomizer"; // Capitalized
+import NonAprealCustomizer from "./components/nonAprealCustomizer";
+import CustomiseTab from "./components/CustomiseTab"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function Root() {
+  return (
+    <>
+      <CustomiseTab />
+      <Routes>
+        <Route path="/apreal" element={<AprealCustomizer />} />
+        <Route path="/non-apreal" element={<NonAprealCustomizer />} />
+        {/* Optionally redirect default path */}
+        <Route path="*" element={<AprealCustomizer />} />
+      </Routes>
+    </>
+  );
+}
 
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/apreal" element={<AprealCustomizer />} />
-        <Route path="/non-apreal" element={<NonAprealCustomizer />} />
-      </Routes>
+      <Root />
     </BrowserRouter>
   </React.StrictMode>
 );
-
-reportWebVitals();
